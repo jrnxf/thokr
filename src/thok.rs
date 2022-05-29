c86d4f77a85895da0b5c0e634cafc61310325ca9
Shashwat <10794178+TheTrio@users.noreply.github.com>
2022-05-29 18:08:32 +0530

    Remove redundant condition
    I just realized that even when the number of characters is equal to the max length, the next word won't fit on that line. So there's no need to treat this case separately

--- src/thok.rs
+++ src/thok.rs
@@ -251,21 +251,6 @@ impl Thok {
             return;
         }
         let count = self.cursor_pos - self.total_line_length;
-        
-        // this is for when the number of characters in a line
-        // is exactly equal to the max line length
-        if count == 0 {
-            self.line_lengths.push_back(max_width);
-            // we start skipping lines when the number of lines completed is >= 2
-            // the length of the deque is at most 2 - the line lengths for the previous 2 lines
-            // line length is the actual number of characters in a line 
-            if self.line_lengths.len() >= 2 {
-                // pop the line length of the last line in a FIFO manner
-                self.skip_curr += self.line_lengths.pop_front().unwrap();
-            }
-            self.total_line_length += max_width;
-            return;
-        }
 
         // this is the case when the current position is < the max width
         // but the next word might not fit and may be on the next line
@@ -285,9 +270,11 @@ impl Thok {
                 self.line_lengths.push_back(count);
                 self.total_line_length += count;
             }
-            // same as before - if the number of lines completed is >=2, we need to skip 
-            // the number of characters that occurred in the popped line
+            // we start skipping lines when the number of lines completed is >= 2
+            // the length of the deque is at most 2 - the line lengths for the previous 2 lines
+            // line length is the actual number of characters in a line
             if self.line_lengths.len() >= 2 {
+                // pop the line length of the last line in a FIFO manner
                 self.skip_curr += self.line_lengths.pop_front().unwrap();
             }
         }
